a9a1f22582955856332a19699adac20aaba663fd
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2020-10-25 17:20:13 +0100

    creation du script de la base de données mysql

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+print("Bienvenus dans l'application Bien Manger")
+
+print("Menus")
+
+choix1 = print(f"1-Quel aliment souhaitez vous remplacer ?")
+choix2 = print(f"2-Retrouver mes aliments substitués")
+
+choixMenus = input("Si vous souhaiter remplacer une aliment tapez 1,\n Si vous souhaiter retrouver vos aliments substités tapez 2 \n")
+
+if choixMenus == "1":
+    categorie = []
+    print(f"Selectionner une categorie: ")
+    
